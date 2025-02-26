73e26e7b37e5ac35bb3d6ac01f03bb6b7f6f13df
fedexcc <fedexcc@icloud.com>
2025-02-26 02:59:30 -0300

    Implement components

--- src/App.tsx
+++ src/App.tsx
@@ -3,39 +3,43 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './styles/App.scss'
 import Button from './components/Button'
+import TopBar from './components/TopBar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <Button 
-          text={`count is ${count}`} 
-          onClick={() => setCount((count) => count + 1)}
-          variant="primary"
-        />
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+      <TopBar />
+      <main className="app-content">
+        <div>
+          <a href="https://vite.dev" target="_blank">
+            <img src={viteLogo} className="logo" alt="Vite logo" />
+          </a>
+          <a href="https://react.dev" target="_blank">
+            <img src={reactLogo} className="logo react" alt="React logo" />
+          </a>
+        </div>
+        <h1>Vite + React</h1>
+        <div className="card">
+          <Button 
+            text={`count is ${count}`} 
+            onClick={() => setCount((count) => count + 1)}
+            variant="primary"
+          />
+          <p>
+            Edit <code>src/App.tsx</code> and save to test HMR
+          </p>
+        </div>
+        <div className="button-showcase">
+          <Button text="Primary Button" variant="primary" />
+          <Button text="Secondary Button" variant="secondary" />
+          <Button text="Outline Button" variant="outline" />
+        </div>
+        <p className="read-the-docs">
+          Click on the Vite and React logos to learn more
         </p>
-      </div>
-      <div className="button-showcase">
-        <Button text="Primary Button" variant="primary" />
-        <Button text="Secondary Button" variant="secondary" />
-        <Button text="Outline Button" variant="outline" />
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      </main>
     </>
   )
 }
