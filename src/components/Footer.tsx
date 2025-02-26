616e433d1720ad1ccc99e3ed48868496676712dd
fedexcc <fedexcc@icloud.com>
2025-02-26 03:17:28 -0300

    WIP Footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../styles/Footer.scss';
+
+const Footer: React.FC = () => {
+  return (
+    <footer className="footer">
+    </footer>
+  );
+};
+
+export default Footer; 
