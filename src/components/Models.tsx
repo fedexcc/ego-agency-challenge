ad94073728192c2e49899a00b4d1ec92c0fcf253
fedexcc <fedexcc@icloud.com>
2025-02-26 04:14:14 -0300

    Implement FilterSection inside Models

---
+++ src/components/Models.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import FilterSection from './FilterSection';
+
+const Models: React.FC = () => {
+  return (
+    <div className="models-container">
+      <h1>Descubrí todos los modelos</h1>
+      <FilterSection />
+      {/* Content will be added later */}
+    </div>
+  );
+};
+
+export default Models; 
